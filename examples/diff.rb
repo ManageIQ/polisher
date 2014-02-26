eca63a8f668f2a9dbcb757708228302172a35c0f
Mo Morsi <mmorsi@redhat.com>
2014-02-26 15:47:58 -0500

    Fix variable name in examples/diff.rb

--- examples/diff.rb
+++ examples/diff.rb
@@ -3,8 +3,8 @@
 require 'polisher/rpm/patch'
 require 'polisher/gem'
 
-rails = Polisher::Gem.from_rubygems 'fog', '1.19.0'
+fog = Polisher::Gem.from_rubygems 'fog', '1.19.0'
 other = Polisher::Git::Repo.new :url => 'https://github.com/ManageIQ/fog.git'
 other.clone unless other.cloned?
 
-puts rails.diff(other)
+puts fog.diff(other)
