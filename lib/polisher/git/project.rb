94b89b73ae769486bb1d2c81bbece43f06ff4439
Mo Morsi <mmorsi@redhat.com>
2014-11-26 12:53:03 -0500

    Update git modules to pull in new paths

--- lib/polisher/git/project.rb
+++ lib/polisher/git/project.rb
@@ -3,7 +3,7 @@
 # Licensed under the MIT license
 # Copyright (C) 2013-2014 Red Hat, Inc.
 
-require 'polisher/vendor'
+require 'polisher/mixins/vendored_deps'
 require 'polisher/git/repo'
 
 module Polisher
