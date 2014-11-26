e40ee9726b3237cc146062b45b9e5ab2b324d20f
Mo Morsi <mmorsi@redhat.com>
2014-11-26 12:57:05 -0500

    Move Polisher::Bugzilla into the targets directory

--- lib/polisher/targets/bugzilla.rb
+++ lib/polisher/targets/bugzilla.rb
@@ -3,7 +3,7 @@
 # Licensed under the MIT license
 # Copyright (C) 2013-2014 Red Hat, Inc.
 
-# TODO use ruby_bugzilla to search to packages in bz
+# TODO: use ruby_bugzilla to search to packages in bz
 
 module Polisher
   class Bugzilla
