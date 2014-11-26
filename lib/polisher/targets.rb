59332e4eeab690c05aa69a6de94c6dc4af77356a
Mo Morsi <mmorsi@redhat.com>
2014-11-26 13:01:31 -0500

    Cleanup few outstanding modules
    - Removed gem_state, previously added to the gem directory
    
    - Remove git.rb, uneeded
    
    - adds targets.rb including all currently defined targets

---
+++ lib/polisher/targets.rb
@@ -0,0 +1,13 @@
+# Polisher Targets
+#
+# Licensed under the MIT license
+# Copyright (C) 2013-2014 Red Hat, Inc.
+
+require 'polisher/apt'
+require 'polisher/bodhi'
+require 'polisher/bugzilla'
+require 'polisher/errata'
+require 'polisher/fedora'
+require 'polisher/koji'
+require 'polisher/rhn'
+require 'polisher/yum'
