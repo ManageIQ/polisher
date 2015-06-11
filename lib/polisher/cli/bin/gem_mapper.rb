1499dee4f00d46fda49a94423edfa60a42f67f41
Mo Morsi <mmorsi@redhat.com>
2015-06-11 17:46:29 -0400

    Refactor / simplify gem_mapper utility module

--- lib/polisher/cli/bin/gem_mapper.rb
+++ lib/polisher/cli/bin/gem_mapper.rb
@@ -24,32 +24,14 @@ def print_dep(gem, dep, resolved_dep)
   print pretty_dep(gem, dep, resolved_dep)
 end
 
-def print_gem_deps(gem)
-  gem.dependency_tree(:recursive => true,
-                      :dev_deps  => conf[:devel_deps],
-                      :matching  => conf[:matching]) do |gem, dep, resolved_dep|
-    print_dep(gem, dep, resolved_dep)
-  end
-end
-
-def print_gemfile_deps(gemfile)
-  gemfile.dependency_tree(:recursive => true,
-                          :dev_deps  => conf[:devel_deps],
-                          :matching  => conf[:matching]) do |gem, dep, resolved_dep|
+def print_deps(source)
+  source.dependency_tree(:recursive => true,
+                         :dev_deps  => conf[:devel_deps],
+                         :matching  => conf[:matching]) do |gem, dep, resolved_dep|
     print_dep(gem, dep, resolved_dep)
   end
 end
 
 def print_tree(conf)
-  if conf_gem?
-    print_gem_deps(conf_source)
-
-  elsif conf_gemfile?
-    print_gemfile_deps(conf_source)
-
-  end
-
-  # XXX
-  puts last_dep
-  puts last_gem
+  print_deps(conf_source) if conf_gem? || conf_gemfile?
 end
