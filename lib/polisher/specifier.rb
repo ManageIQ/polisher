1b2d01652a835ae3e7b165d17abe718ef101147d
Mo Morsi <mmorsi@redhat.com>
2015-06-11 17:50:04 -0400

    Add specifier module

---
+++ lib/polisher/specifier.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# Polisher Gem Specifier Options
+#
+# Licensed under the MIT license
+# Copyright (C) 2015 Red Hat, Inc.
+###########################################################
+
+module Polisher
+  EARLIEST_SPECIFIER  =  :earliest
+  LATEST_SPECIFIER    =  :latest
+
+  RELATIVE_SPECIFIERS = [EARLIEST_SPECIFIER, LATEST_SPECIFIER]
+end # module Polisher
